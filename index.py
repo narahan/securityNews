828bf4d4505001b2fde451ee349063b200631205
narahan <narahan1006@gmail.com>
2018-12-25 23:14:05 +0900

    get url list

--- index.py
+++ index.py
@@ -6,13 +6,38 @@ from bs4 import BeautifulSoup as bss
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-url = 'https://www.boannews.com/media/list.asp?mkind=1'
+base_url =' https://www.boannews.com'
+
+
+url = base_url + '/media/list.asp?mkind=1'
+
+# base url
+
+
+
 f = urlopen(url)
 b = f.read()
 soup = bss(b, 'html.parser')
-divs = soup.find_all('div',{'class': 'news_main_title'})
 
+#main = soup.find_all('div', {'id': 'news_area'})
+
+
+
+
+divs = soup.find_all('div',{'class': 'news_list'})
 
 print('==============================================')
+#print(divs)
 
-print(divs)
+
+# get title list
+for i in divs:
+	titleArr = i.find_all('span', {'class': 'news_txt'})
+	#print(titleArr)
+
+# get href lis
+for j in divs:
+	aa = j.find('a')['href']
+	print(base_url + aa)
+
+
