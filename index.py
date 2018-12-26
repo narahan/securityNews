586014aee6fad9f16dc5999496561822cb80730e
narahan <narahan1006@gmail.com>
2018-12-26 15:17:39 +0900

    modify index file

--- index.py
+++ index.py
@@ -6,37 +6,33 @@ from bs4 import BeautifulSoup as bss
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
+
+# Boannews Base URL
 base_url =' https://www.boannews.com'
 
 
 url = base_url + '/media/list.asp?mkind=1'
 
-# base url
-
-
-
 f = urlopen(url)
 b = f.read()
 soup = bss(b, 'html.parser')
 
-#main = soup.find_all('div', {'id': 'news_area'})
-
-
-
-
 divs = soup.find_all('div',{'class': 'news_list'})
 
 print('==============================================')
-#print(divs)
 
 
-# get title, url list
-for i in divs:
-	#titleArr = i.find_all('span', {'class': 'news_txt'})
-	titleArr = i.find_all('span')[0]
-	print(titleArr.string)
+try:
+	# get title, url list
+	for i in divs:
+		#titleArr = i.find_all('span', {'class': 'news_txt'})
+		titleArr = i.find_all('span')[0]
+		print(titleArr.string)
 
-	urlArr = i.find('a')['href']
-	print(base_url + urlArr)
-	
-
+		urlArr = i.find('a')['href']
+		print(base_url + urlArr)
+# test	
+except urllib.error.HTTPError as e:
+	print(e.code)
+	print(e.reason)
+	print(e.headers)
