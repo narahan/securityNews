66a9386df5f8373d870230baed58a78878a2246e
narahan <narahan1006@gmail.com>
2019-05-10 15:06:06 +0900

    수정

--- index.py
+++ index.py
@@ -2,14 +2,20 @@
 
 
 # flask 모듈에서 Flask 클래스 임포트
-from flask import Flask
+from flask import Flask, render_template
+from data import Articles
+import sqlite3
 
 # Flask 객체의 인스턴스를 만들고 'app'에 할당	 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return 'Flask Test'
 
-app.run()
+@app.route('/')
+def index():
+	return render_template('index.html') 
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
 
