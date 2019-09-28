a5dbf6ccf0b42c87cad217181b1ec410ca0d088a
narahan <narahan1006@gmail.com>
2019-09-29 02:15:53 +0900

    get data boannews

--- index.py
+++ index.py
@@ -3,17 +3,24 @@
 
 # flask 모듈에서 Flask 클래스 임포트
 from flask import Flask, render_template
-from data import Articles
-import sqlite3
+from boannews import getData
 
-# Flask 객체의 인스턴스를 만들고 'app'에 할당	 
+
+
+
+# Flask 객체의 인스턴스를 만들고 'app'에 할당
 app = Flask(__name__)
+
+
+
+# 보안뉴스 크롤링 데이터
+boan_data = getData()
+
 
 
 @app.route('/')
 def index():
-	return render_template('index.html') 
-
+	return render_template('index.html', data = boan_data) 
 
 
 if __name__ == '__main__':
