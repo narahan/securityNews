5ee9f22b4dd49e925bc74e6e810f5b15a42ef172
narahan <narahan1006@gmail.com>
2020-01-09 21:52:19 +0900

    뉴스 주소 변경

--- main/boannews.py
+++ main/boannews.py
@@ -11,57 +11,71 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 
 # Boannews Base URL
-base_url ='https://www.boannews.com'
+base_url ='https://www.boannews.com/media/list.asp?mkind=1'
+	   # ='https://www.boannews.com/media/list.asp?mkind=1'
+# https://www.boannews.com/media/view.asp?idx=85674
 
-#url = base_url + '/media/list.asp?mkind=1'
-url = base_url + '/search/news_list.asp?search=key_word&find=%BB%E7%B0%C7%BB%E7%B0%ED'
-#https://www.boannews.com/search/news_list.asp?search=key_word&find=%BB%E7%B0%C7%BB%E7%B0%ED
 
-f = urlopen(url)
+# url = base_url + 'idx=85674'
+# url = base_url + 'view.asp?idx='
+# url = 'https://www.boannews.com/media/view.asp?idx='
+
+
+
+f = urlopen(base_url)
 b = f.read()
 soup = bss(b, 'html.parser')
 
 
 
+# divs = soup.find_all('div', { 'id': 'news_area' })
+divs = soup.find_all('div', {'class' : 'news_list'})
+
+# divs = soup.find_all('div', { 'class': 'news_main_title' })
 
 
-divs = soup.find_all('div', { 'class': 'news_list' })
+
+# To Do!!!
+# title, url, number 가져오기
 
 
-file_data = []
-def getBoanData():
-	num = 0
-	for i in divs:
 
-		f = {}
-		title = i.find_all('span', {'class': 'news_txt'})
-		title = i.find_all('span')[0]
-		title = title.string
+print()
+# file_data = []
+# def getBoanData():
+# 	num = 0
+# 	for i in divs:
 
-		url = i.find('a')['href']
-		url = base_url + url
+# 		f = {}
+# 		title = i.find_all('span', {'class': 'news_txt'})
+# 		title = i.find_all('span')[0]
+# 		title = title.string
 
-		date = i.find_all('span', {'class': 'news_writer'})
-		# ex) <span class="news_writer">권준  기자 | 2019.08.12 12:02</span>
+# 		url = i.find('a')['href']
+# 		url = base_url + url
 
-		date = i.find_all('span')[1]
-		# ex) 2019.08.12 12:02
-		date = date.string.partition('|')[2]
-		# ex) 2019.08.12
-		date = date.split(' ')[1]
+# 		date = i.find_all('span', {'class': 'news_writer'})
+# 		# ex) <span class="news_writer">권준  기자 | 2019.08.12 12:02</span>
 
-		num = num + 1
-		f['num'] = num
-		f['date'] = date
-		f['title'] = title
-		f['url'] = url
+# 		date = i.find_all('span')[1]
+# 		# ex) 2019.08.12 12:02
+# 		date = date.string.partition('|')[2]
+# 		# ex) 2019.08.12
+# 		date = date.split(' ')[1]
+
+# 		num = num + 1
+# 		f['num'] = num
+# 		f['date'] = date
+# 		f['title'] = title
+# 		f['url'] = url
 		
 
-		file_data.append(f)
+# 		file_data.append(f)
 		
-	return file_data
+# #	return file_data
+# 	print(file_data)
 
-
+# getBoanData()
 
 
 
